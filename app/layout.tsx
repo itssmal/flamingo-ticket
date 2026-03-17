a26ed4cebb27df25214dda0b0bcdeb30953f7973
Oleksandr Smal <its.ssmal@gmail.com>
2026-03-17 17:43:16 +0200

    feat: update metadata, icon. add default redirect from /

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,21 @@
-import type { Metadata } from "next";
-import { Geist } from "next/font/google";
-import { Providers } from "@/components/layout/providers";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
+import { Providers } from '@/components/layout/providers';
+import './globals.css';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+const defaultUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : 'http://localhost:3000';
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: 'Flamingo Ticket management system',
+  description:
+    'A multi-tenant client portal where technicians can manage support tickets for different client organizations.',
 };
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  display: "swap",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  display: 'swap',
+  subsets: ['latin'],
 });
 
 export default function RootLayout({
